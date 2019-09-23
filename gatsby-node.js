6036bb66ef005136decfc792dd7a4163d4014ffb
Tanya Brassie <tbrassie@empatico.org>
2019-09-23 17:22:31 -0400

    Disable trailing slashes

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const path = require('path');
 exports.onCreateNode = ({node, getNode, actions}) => {
   const {createNodeField} = actions;
   if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({node, getNode, basePath: 'content'});
+    const slug = createFilePath({node, getNode, basePath: 'content', trailingSlash: false});
     createNodeField({
       node,
       name: 'slug',
