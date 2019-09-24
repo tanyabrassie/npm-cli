027420e5528bba3c2d4aea6d3655d3b9c500b179
Tanya Brassie <tbrassie@empatico.org>
2019-09-24 08:34:32 -0400

    Footer background colors

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -6,12 +6,14 @@ import {Flex, Box} from 'rebass';
 const Container = styled(Flex)`
   background: center / cover no-repeat url(${boxes}); 
   height: 380px;
+  background-color: ${(props) => props.theme.colors.offWhite};
  `;
 
 const ContentWrapper = styled(Box)`
   align-content: center;
   width: 100%;
   text-align: center;
+  background-color: ${(props) => props.theme.colors.white};
 `;
 
 const Footer = () => {
